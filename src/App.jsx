8c8dc007057ef12ae13d5ac61655b094eb38080a
KGandhi90 <kevin.gandhi@somaiya.edu>
2026-05-14 19:57:34 +0530

    Added MyBooking Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Experts from './components/Experts'
 import ExpertDetails from './components/ExpertDetails'
 import Booking from './components/Booking'
+import MyBookings from './components/MyBookings'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Experts />} />
         <Route path="/expertDetails" element={<ExpertDetails />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/myBookings" element={<MyBookings />} />
         {/* <Route path="*" element={<NotFound />} /> 404 catch-all */}
       </Routes>
     </>
